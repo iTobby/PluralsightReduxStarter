5a39b769592ace57a1965213f4076343819047e8
Tobby Olusanya <Tobby@iTobby-Mac.local>
2016-06-02 12:34:46 +0200

    Authors and Ajax added

--- src/actions/courseActions.js
+++ src/actions/courseActions.js
@@ -1,18 +1,40 @@
 import * as types from './actionTypes';
 import courseApi from '../api/mockCourseApi';
+import {beginAjaxCall} from './ajaxStatusActions';
 
 export function loadCoursesSuccess(courses){
-  return {
-    type: types.LOAD_COURSES_SUCCESS,
-    courses //ES6 Short Hand Property Name
-  };
+  return { type: types.LOAD_COURSES_SUCCESS, courses };
+}
+
+export function updateCourseSuccess(course){
+  return {type: types.UPDATE_COURSE_SUCCESS, course };
+}
+
+export function createCourseSuccess(course){
+  return { type: types.CREATE_COURSE_SUCCESS, course };
 }
 
 export function loadCourses() {
   return function(dispatch) {
+    dispatch(beginAjaxCall());
     return courseApi.getAllCourses()
     .then(courses => {
       dispatch(loadCoursesSuccess(courses));
+    })
+    .catch(err => {
+      throw(err);
+    });
+  };
+}
+
+export function saveCourse(course) {
+  return function(dispatch) {
+    dispatch(beginAjaxCall());
+    return courseApi.saveCourse(course)
+    .then(course => {
+      dispatch(course.id ?
+        updateCourseSuccess(course) :
+        createCourseSuccess(course));
     })
     .catch(err => {
       throw(err);
--- src/components/course/CoursesPage.js
+++ src/components/course/CoursesPage.js
@@ -3,11 +3,13 @@ import { connect } from 'react-redux';
 import { bindActionCreators } from 'redux';
 import * as courseActions from '../../actions/courseActions';
 import CourseList from './CourseList';
+import {browserHistory} from 'react-router';
 
 class CoursesPage extends Component {
 
   constructor(props, context) {
     super(props, context);
+    this.redirectToAddCoursePage = this.redirectToAddCoursePage.bind(this);
   }
 
   courseRow(course, index){
@@ -16,12 +18,21 @@ class CoursesPage extends Component {
     );
   }
 
+  redirectToAddCoursePage(){
+    browserHistory.push('/course');
+  }
+
   render() {
     const {courses} = this.props;
 
     return(
       <div>
         <h1>Courses</h1>
+      <input
+            type="submit"
+            value="Add Course"
+            className="btn btn-primary"
+            onClick={this.redirectToAddCoursePage}/>
         <CourseList courses={courses} />
       </div>
     );
