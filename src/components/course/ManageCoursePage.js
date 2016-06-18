6990e46455c05c23257de70d45a0699bec1badbb
unknown <TobbyO@Dev11k.corp.faranani.com>
2016-06-18 14:42:22 +0200

    Added more tests (reducer, thunk and selector)

--- src/components/course/ManageCoursePage.js
+++ src/components/course/ManageCoursePage.js
@@ -3,6 +3,7 @@ import { connect } from 'react-redux';
 import { bindActionCreators } from 'redux';
 import * as courseActions from '../../actions/courseActions';
 import CourseForm from './CourseForm';
+import {authorsFormattedForDropdown} from '../../selectors/selectors';
 import toastr from 'toastr';
 
 export class ManageCoursePage extends React.Component {
@@ -87,7 +88,7 @@ ManageCoursePage.propTypes = {
 };
 
 
-//Pull in the React Router context so router is available on this.context.router
+// Pull in the React Router context so router is available on this.context.router
 ManageCoursePage.contextTypes = {
   router: PropTypes.object.isRequired
 };
@@ -107,16 +108,10 @@ function mapStateToProps(state, ownProps){
     course = getCourseById(state.courses, courseId);
   }
 
-  const authorsFormattedForDropdown = state.authors.map(author => {
-    return {
-      value: author.id,
-      text: author.firstName + ' ' + author.lastName
-    };
-  });
 
   return {
     course: course,
-    authors: authorsFormattedForDropdown
+    authors: authorsFormattedForDropdown(state.authors)
   };
 }
 
